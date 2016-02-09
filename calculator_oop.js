2c81c10a0f3ef126c65e05b9c1bdb174fbe7ec1a
michpenn <michpenn@icloud.com>
2016-02-09 10:49:31 -0800

    trial and error OOP

---
+++ calculator_oop.js
@@ -0,0 +1,14 @@
+/**
+ * Created by michpenn on 1/26/16.
+ */
+
+//constructor for all possible inputs
+var Input = function (value, type, hasDecimal, hasPrecedence) {
+    this.value = value;
+    this.type = type;
+    this.hasDecimal = hasDecimal;
+    this.hasPrecedence = hasPrecedence;
+};
+
+
+$(document).ready(function() {});
